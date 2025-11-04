d4025ffe274fff92662dc003fd7ebc822c0ebe76
ikerbosquez05 <ikerbosquez05@gmail.com>
2025-11-04 08:11:13 +0100

    primera version

---
+++ src/principal/HolaMundo.java
@@ -0,0 +1,14 @@
+package principal;
+
+import java.util.Scanner;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce una letra: ");
+		String respuesta = sc.next();
+		
+	}
+
+}
